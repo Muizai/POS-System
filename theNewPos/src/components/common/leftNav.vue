d62b4d086508ab9688c13f3323580f1bce0fa6af
huangxiaoling <huangxiaoling28@qq.com>
2018-11-14 10:03:53 +0800

    pos
    pos-system

---
+++ theNewPos/src/components/common/leftNav.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="leftNav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-goumai"></i>
+                <div>收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dianpu"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-hanbao"></i>
+                <div>商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-huiyuanqia"></i>
+                <div>会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-tongji"></i>
+                <div>统计</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-gongnengjianyi"></i>
+                <div>设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .leftNav{
+        background: #1d8ce0;
+        color: #fff;
+        font-size: 10px;
+        width: 5%;
+        float: left;        
+        height: 100%;
+    }
+    .leftNav ul{
+        margin: 0;
+        padding: 0;
+    }
+    .leftNav li{
+        list-style: none;
+        padding: 10px;
+        border-bottom: 1px solid #ccc;
+        text-align: center;
+
+    }
+    .icon{
+        font-size: 26px;
+    }
+</style>
